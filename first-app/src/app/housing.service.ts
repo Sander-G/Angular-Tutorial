76cc85013d9a8aa1d0cacb5e46464993bfd50dbd
Sander - iMac <Sander@Grunnekemeijer.net>
2023-05-11 11:33:35 +0200

    add a form

--- first-app/src/app/housing.service.ts
+++ first-app/src/app/housing.service.ts
@@ -113,7 +113,12 @@ export class HousingService {
   }
 
   getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);  
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application recieved: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
   constructor() { }
 }
